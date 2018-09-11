ecc9b1fc5a1b257b878f20ca2cfb3adfe3cafd7b
Alexander Evstefeev <aevstefeev@pushwoosh.com>
2018-09-11 19:00:47 +0700

    [feature][PUSH-15490] add dependencies for android inbox

--- Source/pushwoosh_inbox/android/src/main/kotlin/com/pushwoosh/pushwooshinbox/PushwooshInboxPlugin.kt
+++ Source/pushwoosh_inbox/android/src/main/kotlin/com/pushwoosh/pushwooshinbox/PushwooshInboxPlugin.kt
@@ -1,10 +1,13 @@
 package com.pushwoosh.pushwooshinbox
 
+import android.content.Intent
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import com.pushwoosh.internal.platform.AndroidPlatformModule
+import com.pushwoosh.inbox.ui.presentation.view.activity.InboxActivity
 
 class PushwooshInboxPlugin(): MethodCallHandler {
   companion object {
@@ -16,6 +19,11 @@ class PushwooshInboxPlugin(): MethodCallHandler {
   }
 
   override fun onMethodCall(call: MethodCall, result: Result): Unit {
+    if(call.method.equals("presentInboxUI")){
+      val applicationContext = AndroidPlatformModule.getApplicationContext()
+      applicationContext!!.startActivity(Intent(applicationContext, InboxActivity::class.java))
+        result.notImplemented()
+    }
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
     } else {
