ff62d1cecb7b347eccd5284167d2bf16b4bb2af5
Sergey Polshcha <fectum@pushwoosh.com>
2018-09-14 11:29:18 +0700

    [PUSH-15491] comments for documentation

--- Source/pushwoosh_inbox/lib/pushwoosh_inbox.dart
+++ Source/pushwoosh_inbox/lib/pushwoosh_inbox.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+/// This class is designed to customize the Inbox appearance
 class PWInboxStyle {
 
   /// Inbox message date format. For example: "dd.MMMM.yyyy"
@@ -10,13 +11,13 @@ class PWInboxStyle {
   /// The default icon in the cell next to the message; if not specified, the app icon is used
   String defaultImage;
 
-  /// The appearance of the unread messages mark
+  /// The appearance of the unread messages mark (iOS only)
   String unreadImage;
 
   /// The image which is displayed if an error occurs and the list of inbox messages is empty
   String listErrorImage;
 
-  /// The text which is displayed if the list of inbox messages is empty; cannot be localized
+  /// The image which is displayed if the list of inbox messages is empty
   String listEmptyImage;
 
   /// The error text which is displayed when an error occurs; cannot be localized
@@ -24,32 +25,54 @@ class PWInboxStyle {
 
   /// The text which is displayed if the list of inbox messages is empty; cannot be localized
   String listEmptyMessage;
+
+  /// The default bar title text
   String barTitle;
 
+  /// The accent color
   String accentColor;
+
+  /// The default background color
   String backgroundColor;
+
+  /// The default selection color
   String highlightColor;
 
+  /// The default text color (iOS only)
   String defaultTextColor;
 
   String imageTypeColor;
   String readImageTypeColor;
 
+  /// The color of message titles
   String titleColor;
+
+  /// The color of message titles if message was readed (Android only)
   String readTitleColor;
 
+  /// The color of messages descriptions
   String descriptionColor;
+
+  /// The color of messages descriptions if message was readed (Android only)
   String readDescriptionColor;
 
+  /// The color of message dates
   String dateColor;
+
+  /// The color of message dates if message was readed (Android only)
   String readDateColor;
 
+  /// The color of the separator
   String dividerColor;
 
+  /// The default bar color
   String barBackgroundColor;
-  String barAccentColor;
-  String barTextColor;
 
+  /// The default back button color
+  String barAccentColor;
+
+  /// The default bar accent color
+  String barTextColor;
 
   Map<String, dynamic> _dictionaryRepresentation() {
     Map<String, dynamic> params = Map();
@@ -154,76 +177,9 @@ class PWInboxStyle {
   }
 }
 
-// //! The default icon in the cell next to the message; if not specified, the app icon is used
-// @property (nonatomic, readwrite) UIImage *defaultImageIcon;
-
-// //! The default font
-// @property (nonatomic, readwrite) UIFont *defaultFont;
-
-// //! The default text color
-// @property (nonatomic, readwrite) UIColor *defaultTextColor;
-
-// //! The default background color
-// @property (nonatomic, readwrite) UIColor *backgroundColor;
-
-// //! The default selection color
-// @property (nonatomic, readwrite) UIColor *selectionColor;
-
-// //! The appearance of the unread messages mark
-// @property (nonatomic, readwrite) UIImage *unreadImage;
-
-// //! The image which is displayed if an error occurs and the list of inbox messages is empty
-// @property (nonatomic, readwrite) UIImage *listErrorImage;
-
-// //! The error text which is displayed when an error occurs; cannot be localized
-// @property (nonatomic, readwrite) NSString *listErrorMessage;
-
-// //! The image which is displayed if the list of inbox messages is empty
-// @property (nonatomic, readwrite) UIImage *listEmptyImage;
-
-// //! The text which is displayed if the list of inbox messages is empty; cannot be localized
-// @property (nonatomic, readwrite) NSString *listEmptyMessage;
-
-// //! The accent color
-// @property (nonatomic, readwrite) UIColor *accentColor;
-
-// //! The color of message titles
-// @property (nonatomic, readwrite) UIColor *titleColor;
-
-// //! The color of messages descriptions
-// @property (nonatomic, readwrite) UIColor *descriptionColor;
-
-// //! The color of message dates
-// @property (nonatomic, readwrite) UIColor *dateColor;
-
-// //! The color of the separator
-// @property (nonatomic, readwrite) UIColor *separatorColor;
-
-// //! The font of message titles
-// @property (nonatomic, readwrite) UIFont *titleFont;
-
-// //! The font of message descriptions
-// @property (nonatomic, readwrite) UIFont *descriptionFont;
-
-// //! The font of message dates
-// @property (nonatomic, readwrite) UIFont *dateFont;
-
-// //! The default bar color
-// @property (nonatomic, readwrite) UIColor *barBackgroundColor;
-
-// //! The default back button color
-// @property(nonatomic, readwrite) UIColor *barAccentColor;
-
-// //! The default bar accent color
-// @property (nonatomic, readwrite) UIColor *barTextColor;
-
-// //! The default bar title text
-// @property (nonatomic, readwrite) NSString *barTitle;
-
 /// Implementation of the Pushwoosh Inbox API for Flutter.
 class PushwooshInbox {
   static const MethodChannel _channel = const MethodChannel('pushwoosh_inbox');
-
 
   /// Present Inbox UI
   static void presentInboxUI({PWInboxStyle style}) {
